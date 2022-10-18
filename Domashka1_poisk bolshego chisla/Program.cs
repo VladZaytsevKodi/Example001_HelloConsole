eeb8d96fbdd21f2014fb6760347ca30178fe4b87
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-18 15:58:36 +0300

    поиск большего и меньшего числа

---
+++ Domashka1_poisk bolshego chisla/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напиштитепрограмму, которая на вход принимает два числа и выдаёт, какое число большее,
+// а какое меньшее.
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+if(num1>num2)
+{
+    Console.WriteLine("первое число большее");
+    Console.WriteLine("второе число меньшее");
+}
+else
+{
+    Console.WriteLine("второе число большее");
+    Console.WriteLine("первое число меньшее");
+}
